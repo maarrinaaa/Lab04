3a866da23c4682c7be34c7bbfe97f94746cb3e4f
Марина Валерьевна Гончаренко <367160@niuitmo.ru>
2023-12-09 18:04:34 +0300

    Добавьте файлы проекта.

---
+++ Lab04/ILibrary.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Lab04;
+
+public interface ILibrary
+{
+    public IEnumerable<Book> SearchByTitle(string title);
+    public IEnumerable<Book> SearchByAuthor(string author);
+    public IEnumerable<Book> SearchByISBN(string isbn);
+    public IEnumerable<Book> SearchByTags(IEnumerable<string> tags);
+}
---
+++ Lab04/Program.cs
@@ -0,0 +1,39 @@
+﻿using Lab04;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var library = new Library();
+var bookBuilder = new BookBuilder();
+var book = bookBuilder
+    .SetTitle("The Lord of the Rings")
+    .SetAuthor("J. R. R. Tolkien")
+    .SetGenres(new[] { "Fantasy", "Adventure" })
+    .SetISBN("978-0-618-57498-4")
+    .SetPublicationDate(new DateTime(1954, 7, 29))
+    .SetAnnotation("The Lord of the Rings is an epic high-fantasy novel written by English author and scholar J. R. R. Tolkien.")
+    .SetTags(new[] { "fantasy", "adventure", "classic" })
+    .Build();
+library.AddBook(book);
+var secondBookBuilder = new BookBuilder();
+var secondBook = secondBookBuilder
+    .SetTitle("The Hobbit")
+    .SetAuthor("J. R. R. Tolkien")
+    .SetGenres(new[] { "Fantasy", "Adventure" })
+    .SetISBN("978-0-618-57498-5")
+    .SetPublicationDate(new DateTime(1937, 9, 21))
+    .SetAnnotation("The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien.")
+    .SetTags(new[] { "fantasy", "adventure", "classic" })
+    .Build();
+library.AddBook(secondBook);
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/books", () => library.GetCatalog());
+app.MapGet("/books/title/{title}", (string title) => library.SearchByTitle(title));
+app.MapGet("/books/author/{author}", (string author) => library.SearchByAuthor(author));
+app.MapGet("/books/isbn/{isbn}", (string isbn) => library.SearchByISBN(isbn));
+app.MapGet("/books/tags/{tags}", (IEnumerable<string> tags) => library.SearchByTags(tags));
+app.MapPost("/books", (Book book) => library.AddBook(book));
+
+app.Run();
